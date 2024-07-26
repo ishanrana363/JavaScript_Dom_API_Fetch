b8482098fdf68eb82a12d36d4aa7e8c904cf4f06
ishanrana363 <ishanrana363@gmail.com>
2024-07-26 19:13:39 +0600

    JavaScript Dom Selector

---
+++ JavaScript_Dom_Selector/js/main.js
@@ -0,0 +1,11 @@
+// const getH2Tags = document.getElementsByTagName( "span" );
+// console.log(getH2Tags);
+
+// for( let i=0;i<getH2Tags.length;i++ ){
+//     console.log(getH2Tags[i]);
+// };
+
+
+const result = document.getElementsByClassName("helloClass");
+
+console.log(result);
