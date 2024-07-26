2b30e6801b8d47dc873a392ff1f259513261b6df
ishanrana363 <ishanrana363@gmail.com>
2024-07-26 19:44:24 +0600

    querySelector & querySelectorAll

---
+++ Query_Selector/js/main.js
@@ -0,0 +1,12 @@
+// const result = document.querySelector("#helloClass").innerHTML;
+// console.log(result);
+
+// const result2 = document.querySelector(".litag");
+// console.log(result2);
+
+const result3 = document.querySelectorAll("#helloClass");
+for(let i=0;i<result3.length;i++){
+
+    console.log(result3[i].innerHTML= ` <h1>Programmer ishan</h1> ` );
+
+};
