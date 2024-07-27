b8660aa2f2aa0902853ed8942056e5c2fff25976
ishanrana363 <ishanrana363@gmail.com>
2024-07-27 15:05:12 +0600

    onclick method

---
+++ Onclick_Method/js/js/main.js
@@ -0,0 +1,10 @@
+const btn1 = document.getElementById("btn1");
+btn1.onclick = changeColor();
+
+// btn1.onclick = function mkeRed (){
+//     document.body.style.backgroundColor = 'red'
+// }
+
+function changeColor () {
+    document.body.style.backgroundColor = "blue";
+};
