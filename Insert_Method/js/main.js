5f583b85113c04aae3e154125fa1ee4ea481fabf
ishanrana363 <ishanrana363@gmail.com>
2024-07-27 19:55:30 +0600

    create dynamic menu

--- Insert_Method/js/main.js
+++ Insert_Method/js/main.js
@@ -1,6 +1,18 @@
-const ulTag = document.getElementById("ul-tag");
+// const ulTag = document.getElementById("ul-tag");
 
-let liTag = document.createElement("li");
-liTag.innerHTML = `First`;
+// let liTag = document.createElement("li");
+// liTag.innerHTML = `First`;
 
-ulTag.insertBefore(liTag, ulTag.firstChild );
+// ulTag.insertBefore(liTag, ulTag.firstChild );
+
+
+const menuList = document.getElementById("menu-list");
+const list = ['Home',"About","Terms","Profile","link"]
+
+const makeData = list.map((item)=>{
+    const liTag = document.createElement("li");
+    liTag.textContent = item;
+    return liTag;
+});
+
+menuList.append(...makeData);
